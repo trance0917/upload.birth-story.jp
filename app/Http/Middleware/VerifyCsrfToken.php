8e04c8d32ce1555f53cac01067e9827eac8f2880
hachiya0917 <hachiya@e-metals.net>
2023-11-24 11:44:28 +0900

    a

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/line/webhook/*'
+        '/line/webhook/*',
+        '/github/*'
     ];
 }
