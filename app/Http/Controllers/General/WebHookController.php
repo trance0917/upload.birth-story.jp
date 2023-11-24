d37a3eb49d78b97e5da43c899348f2ad0ed5eb0d
hachiya <kei8@apost.plala.or.jp>
2023-11-24 21:46:32 +0900

    ｃ

--- app/Http/Controllers/General/WebHookController.php
+++ app/Http/Controllers/General/WebHookController.php
@@ -2,9 +2,8 @@
 namespace App\Http\Controllers\General;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Services\LineBotService as LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use App\Services\LineBotService;
+use App\Services\MaternityLineBotService;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use App\Models\MstMaternity;
 use App\Models\LogLineWebhook;
@@ -16,26 +15,31 @@ final class WebHookController extends Controller
         if ($_SERVER['HTTP_HOST']=='local.upload.birth-story.jp:8087') {
             $request->destination = 'U92d0798c12c2fdf7cca244be08611be5';//テスト産婦人科
             $request->destination = 'U521b4fe8ebf2b9d99383034188975e81';//テストマタニティクリニック
-            $request->events = json_decode(<<<JSON
-            [{"type": "message","message": {
-            "type": "text",
-				"id": "483138207442010130","quoteToken": "Ne0LeVWnZDKvQsb5ZZNbVSHy0jehLqIdr16xZvNvwBqd60WIL72P7rCpvFDB_brEfEASjqMHD-ZgX7x9rMs8sa9lcZ2Y4_gLw886MydK8I32-jBKqq5RcETDrcD1mueQGYjlEc-tD0qk1POPzTUdjg",
-				"text": "産院担当"
-			},
-			"webhookEventId": "01HFZX6FZCXQ40FW3M7F94CKK9","deliveryContext": {"isRedelivery": false},
-			"timestamp": 1700804116338,"source": {"type": "user","userId": "U42daeeae41b7d137194c58c41fab5306"},"replyToken": "4bd7423be7be440689c4fcc8c77b4e87","mode": "active"
-		}]
-JSON,true);
+
+            //type:message
+            $request->events = json_decode('[{"type": "message","message": {"type": "text","id": "483138207442010130","quoteToken": "Ne0LeVWnZDKvQsb5ZZNbVSHy0jehLqIdr16xZvNvwBqd60WIL72P7rCpvFDB_brEfEASjqMHD-ZgX7x9rMs8sa9lcZ2Y4_gLw886MydK8I32-jBKqq5RcETDrcD1mueQGYjlEc-tD0qk1POPzTUdjg","text": "産院担当"},"webhookEventId": "01HFZX6FZCXQ40FW3M7F94CKK9","deliveryContext": {"isRedelivery": false},"timestamp": 1700804116338,"source": {"type": "user","userId": "U42daeeae41b7d137194c58c41fab5306"},"replyToken": "4bd7423be7be440689c4fcc8c77b4e87","mode": "active"}]',true);
+
+            //type:follow
+            $request->events = json_decode('[{"mode": "active", "type": "follow", "source": {"type": "user", "userId": "U801f291d7fb7b7a3d102a7b2d31662e9"}, "timestamp": 1700828414699, "replyToken": "38eae19242ac4fc9bea25fc1e5f5cdb0", "webhookEventId": "01HG0MC0Z458YSJ6CYR1E3F5EH", "deliveryContext": {"isRedelivery": false}}]',true);
+
+            //type:unfollow
+//            $request->events = json_decode('[{"mode": "active", "type": "unfollow", "source": {"type": "user", "userId": "U801f291d7fb7b7a3d102a7b2d31662e9"}, "timestamp": 1700828412192, "webhookEventId": "01HG0MBYS4V5C4272Z1DQ1PXZ1", "deliveryContext": {"isRedelivery": false}}]',true);
+
         }
 
+
+
         $mst_maternity = MstMaternity::where('line_destination', $request->destination)->first();
-        
+
         if($mst_maternity){
-            
-            $line_bot_service = new LineBotService(new CurlHTTPClient($mst_maternity->line_message_channel_token),['channelSecret' => $mst_maternity->line_message_channel_secret]);
-            
+
+                $line_bot_service = new MaternityLineBotService($mst_maternity);
+//            $line_bot_service = new LineBotService(new CurlHTTPClient($mst_maternity->line_message_channel_token),['channelSecret' => $mst_maternity->line_message_channel_secret]);
+
             if($request->events){
+
                 foreach($request->events AS $event_key=>$event){
+                    dump($event);
                     $event_type = $event['type'];
                     $log_line_webhook = new LogLineWebhook;
                     $log_line_webhook->type = $event['type'];
@@ -43,8 +47,8 @@ JSON,true);
                     $log_line_webhook->line_user_id = $event['source']['userId'];
                     $log_line_webhook->api_data = $request->all();
                     $log_line_webhook->save();
-                    
-                    
+
+
                     if($event_type=='message'){
                         if($event['message']['type']=='text'){
                             if($event['message']['text']=='産院担当者'){
@@ -63,15 +67,13 @@ JSON,true);
 //                        $profile['pictureUrl']
                         }
                     }elseif($event_type=='follow'){
-                        //$line_bot_service->follow();
+                        //フォロワーを手続き
+                        $line_bot_service->follow($event['source']['userId']);
+
                     }elseif($event_type=='unfollow'){
                         //$line_bot_service->unfollow();
                     }
 
-                    
-                    
-                    
-                    
                 }
             }else{
                 $log_line_webhook = new LogLineWebhook;
@@ -85,20 +87,18 @@ JSON,true);
             $log_line_webhook->api_data = $request->all();
             $log_line_webhook->save();
         }
-
-
     }
 
     public function test(Request $request){
 //        $data = $request->all();
 //        $events = $data['events'];
 
-        $httpClient = new CurlHTTPClient(config('services.line.message.channel_token'));
-        $bot = new LINEBot($httpClient, ['channelSecret' => config('services.line.message.channel_secret')]);
-        $message = "画像が送れるか";
-        $textMessageBuilder = new TextMessageBuilder($message);
-        $response = $bot->pushMessage('Ue10f7778b40a1ba8ca5541d1ae5f5925', $textMessageBuilder);
-        $response = $bot->pushMessage('Ue10f7778b40a1ba8ca5541d1ae5f5925', new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder('https://birth-story.jp/images/index-insta-9.png', 'https://birth-story.jp/images/index-insta-9.png'));
+//        $httpClient = new CurlHTTPClient(config('services.line.message.channel_token'));
+//        $bot = new LINEBot($httpClient, ['channelSecret' => config('services.line.message.channel_secret')]);
+//        $message = "画像が送れるか";
+//        $textMessageBuilder = new TextMessageBuilder($message);
+//        $response = $bot->pushMessage('Ue10f7778b40a1ba8ca5541d1ae5f5925', $textMessageBuilder);
+//        $response = $bot->pushMessage('Ue10f7778b40a1ba8ca5541d1ae5f5925', new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder('https://birth-story.jp/images/index-insta-9.png', 'https://birth-story.jp/images/index-insta-9.png'));
 
         return;
     }
@@ -116,15 +116,15 @@ JSON,true);
 //        dump(exec("cd /var/www/dev.upload.birth-story.jp ; echo 'sakura0917' | sudo -S git pull",$opt, $return_ver));
 //        dump($opt);
 //        dump( '実行結果：'.$return_ver);
-        
+
         //        dump(exec($cmd, $opt, $return_ver));
-//        
+//
 //        dump($opt);
 //        dump( '実行結果：'.$return_ver);
-        
+
 //        $cmd = "echo 'sakura0917' | sudo -S git pull";
 //        dump(exec($cmd, $opt, $return_ver));
-//        
+//
 //        dump($opt);
 //        dump( '実行結果：'.$return_ver);
     }
---
+++ app/Services/MaternityLineBotService.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Services;
+
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use App\Models\MstMaternity;
+use App\Models\TblPatient;
+
+class MaternityLineBotService extends LINEBot
+{
+    /** @var string */
+    private $channelSecret;
+    /** @var HTTPClient */
+    private $httpClient;
+
+    private $mst_maternity;
+
+    public function __construct(MstMaternity $mst_maternity) {
+        $httpClient = new CurlHTTPClient($mst_maternity->line_message_channel_token);
+        $args = ['channelSecret' => $mst_maternity->line_message_channel_secret];
+        parent::__construct($httpClient, $args);
+        $this->httpClient = $httpClient;
+        $this->channelSecret = $args['channelSecret'];
+        $this->mst_maternity = $mst_maternity;
+    }
+
+    // 例：送信されたメッセージを取得するAPI
+    public function getMessageContent($messageId)
+    {
+        return $this->httpClient->get('https://api-data.line.me/v2/bot/message/' . urlencode($messageId) . '/content');
+    }
+
+    // 例：LINEのグループ情報を取得するためのAPI
+    public function getGroupSummary($groupId)
+    {
+        return $this->httpClient->get('https://api.line.me/v2/bot/group/' . urlencode($groupId) .'/summary');
+    }
+
+    public function follow($line_user_id){
+        $tbl_patient = new TblPatient;
+
+        $str_r = substr(str_shuffle('123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPUQRSTUVWXYZ'), 0, 8);
+        dump($str_r);
+        dump($line_user_id);
+
+
+    }
+
+}
