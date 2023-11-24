a84bae13435592c71e055d1bf86273470419e5ee
hachiya0917 <hachiya@e-metals.net>
2023-11-24 17:44:39 +0900

    コミット

---
+++ database/migrations/2023_11_24_084038_drop_column_deleted_at_log_lin_webhooks.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('log_line_webhooks', function (Blueprint $table) {
+            $table->dropColumn('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('log_line_webhooks', function (Blueprint $table) {
+            $table->softDeletes()->default(null)->nullable()->after('api_data')->comment('削除日時(論理削除)');
+        });
+    }
+};
