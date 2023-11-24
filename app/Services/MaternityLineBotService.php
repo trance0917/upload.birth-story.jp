491fb32760e9be407e8569361cb2cc2c3a694185
hachiya <kei8@apost.plala.or.jp>
2023-11-24 23:01:11 +0900

    ｃ

--- app/Services/MaternityLineBotService.php
+++ app/Services/MaternityLineBotService.php
@@ -6,6 +6,8 @@ use LINE\LINEBot\HTTPClient;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use App\Models\MstMaternity;
 use App\Models\TblPatient;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class MaternityLineBotService extends LINEBot
 {
@@ -42,28 +44,48 @@ class MaternityLineBotService extends LINEBot
         $code = '';
         $m = null;
 
-        while(true){
-            $code = substr(str_shuffle('123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPUQRSTUVWXYZ'), 0, 8);
-            $m = TblPatient::withTrashed()->where('code', $code)->first();
-            if($m){
-                //コードの重複
-                continue;
+        DB::beginTransaction();
+        try {
+            while(true){
+                $code = substr(str_shuffle('123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPUQRSTUVWXYZ'), 0, 8);
+                $m = TblPatient::withTrashed()->where('code', $code)->first();
+                if($m){
+                    //コードの重複
+                    continue;
+                }
+                break;
             }
-            break;
+
+            $profile = $this->getProfile($line_user_id)->getJSONDecodedBody();
+
+            $tbl_patient->mst_maternity_id = $this->mst_maternity->mst_maternity_id;
+            $tbl_patient->code = $code;
+            $tbl_patient->line_name = $profile['displayName'];
+            $tbl_patient->line_user_id = $line_user_id;
+            $tbl_patient->save();
+            DB::commit();
+        }catch(\Throwable $e){
+            DB::rollback();
+            Log::error($e);
         }
-        $tbl_patient->mst_maternity_id = $this->mst_maternity->mst_maternity_id;
-        $tbl_patient->code = $code;
-        $tbl_patient->line_name = $this->getProfile($line_user_id)->getJSONDecodedBody()['displayName'];
-        $tbl_patient->line_user_id = $line_user_id;
+        dump($profile['pictureUrl']);
         $tbl_patient->richmenu_id = 'リッチメニューID';
 
-        $tbl_patient->save();
+        //リッチメニューIDを紐づける対応が必要
+
+
     }
     public function unfollow($line_user_id){
-        $tbl_patient = TblPatient::where('line_user_id', $line_user_id)->first();
-        $tbl_patient->richmenu_id=null;
-        $tbl_patient->save();
-        $tbl_patient->delete();
+        $tbl_patients = TblPatient::where('line_user_id', $line_user_id)->get();
+        foreach($tbl_patients AS $tbl_patient_key=>$tbl_patient){
+            //リッチメニューIDを取得して解除＆削除する処理が必要
+            $tbl_patient->richmenu_id=null;
+            $tbl_patient->save();
+            $tbl_patient->delete();
+            dump($tbl_patient->tbl_patient_id);
+        }
+
+
         //todo リッチメニューを消す処理
     }
 }
