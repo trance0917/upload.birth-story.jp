443bbadfd10b6e91ee95eba44d537e68d301f735
hachiya <kei8@apost.plala.or.jp>
2023-11-30 23:54:49 +0900

    ｃ

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,9 +3,10 @@
 @extends('layout')
 @section('meta')@endsection
 @section('contents')
-    <h1>404</h1>
-    <div>
-
+    <h1 class="text-center text-[30px] mt-[30px] leading-none">404</h1>
+    <p class="text-center text-[30px] mt-[10px] leading-none">ページが存在しません</p>
+    <div class="flex flex-cold items-center justify-center mt-[30px]">
+        <p>ページ削除された可能性があります。</p>
     </div>
 @endsection
 
