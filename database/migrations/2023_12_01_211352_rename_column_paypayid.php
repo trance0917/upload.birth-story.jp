3ec55c380d119530c4a255c20fa3d8788c5c6508
hachiya <kei8@apost.plala.or.jp>
2023-12-01 22:01:10 +0900

    ｃ

---
+++ database/migrations/2023_12_01_211352_rename_column_paypayid.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tbl_patients', function (Blueprint $table) {
+            //カラム名変更
+            $table->renameColumn('paypayid', 'amazon_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tbl_patients', function (Blueprint $table) {
+            //カラム名変更
+            $table->renameColumn('amazon_id', 'paypayid');
+        });
+    }
+};
