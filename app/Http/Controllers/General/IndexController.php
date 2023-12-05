21c48b473b616dd05bbf5e388a513073c2442dd4
hachiya0917 <hachiya@e-metals.net>
2023-12-05 16:12:11 +0900

    コミット

--- app/Http/Controllers/General/IndexController.php
+++ app/Http/Controllers/General/IndexController.php
@@ -39,15 +39,40 @@ class IndexController extends Controller
 //        dump($a);
 
         ///////////////////////////////////////////////////////////////////////////////////////////
-
-
-        $maternity_line_bot_service = new MaternityLineBotService($tbl_patient->mst_maternity);
-//        dump($maternity_line_bot_service->getRichMenuList()->getJSONDecodedBody());
         
-        dump(fopen(public_path('images/richmenu/active.png'),'r'));
-        dump(public_path('images/richmenu/active.png'));
-        dump($maternity_line_bot_service->uploadRichMenuImage('richmenu-29f5c95014ae2396ba223167503d672c',public_path('images/richmenu/active.png'),'image/png'));
-        dump($maternity_line_bot_service->linkRichMenu($tbl_patient->line_user_id,'richmenu-29f5c95014ae2396ba223167503d672c'));
+        
+        //画像設定
+//        $cmd = '';
+//        $cmd .= 'curl -v -X POST https://api-data.line.me/v2/bot/richmenu/richmenu-d2b659d16c6bbb94fe74a0c7f4122249/content ';
+//        $cmd .= "-H \"Authorization: Bearer {$tbl_patient->mst_maternity->line_message_channel_token}\" ";
+//        $cmd .= "-H \"Content-Type: image/png\" ";
+//        $cmd .= '-T '.public_path('images/richmenu/active.png');
+//        dump(exec($cmd));
+        ////////////////////////////////////////////////////////////////////////
+
+        
+//        dump(public_path('images/richmenu/active.png'));
+//        dump(filesize(public_path('images/richmenu/active.png')));
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, "https://api-data.line.me/v2/bot/richmenu/richmenu-29f5c95014ae2396ba223167503d672c/conten");
+//        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer {$tbl_patient->mst_maternity->line_message_channel_token}","Content-Type: image/png"));
+//        curl_setopt($ch, CURLOPT_POST, true);
+//        curl_setopt($ch, CURLOPT_INFILE, fopen(public_path('images/richmenu/active.png'),'r'));
+//        curl_setopt($ch, CURLOPT_INFILESIZE, filesize(public_path('images/richmenu/active.png')));
+//       
+//        $result = curl_exec($ch);
+//        dump(filesize(public_path('images/richmenu/active.png')));
+//        dump($result);
+        
+        
+
+//        $maternity_line_bot_service = new MaternityLineBotService($tbl_patient->mst_maternity);
+////        dump($maternity_line_bot_service->getRichMenuList()->getJSONDecodedBody());
+//        
+////        dump(fopen(asset('images/richmenu/active.png'),'r'));
+//        dump(public_path('images/richmenu/active.png'));
+//        dump($maternity_line_bot_service->uploadRichMenuImage('richmenu-29f5c95014ae2396ba223167503d672c',public_path('images/richmenu/active.png'),'image/png'));
+//        dump($maternity_line_bot_service->linkRichMenu($tbl_patient->line_user_id,'richmenu-d2b659d16c6bbb94fe74a0c7f4122249'));
 
         ///////////////////////////////////////////////////////////////////////////////////////////
 //        $bubble_container_builder = new \LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder(
@@ -61,6 +86,10 @@ class IndexController extends Controller
 //        $maternity_line_bot_service = new MaternityLineBotService($tbl_patient->mst_maternity);
 //        $maternity_line_bot_service->pushMessage($tbl_patient->line_user_id,new FlexMessageBuilder('高評価ありがとうございます',$bubble_container_builder),$tbl_patient);
 
+
+
+        $maternity_line_bot_service = new MaternityLineBotService($tbl_patient->mst_maternity);
+        dump($maternity_line_bot_service->unlinkRichMenu($tbl_patient->line_user_id));
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 
