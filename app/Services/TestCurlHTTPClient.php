08700c2d53bef8024191fa01a6e2aedbd7a089ed
hachiya0917 <hachiya@e-metals.net>
2023-12-05 14:34:17 +0900

    コミット

---
+++ app/Services/TestCurlHTTPClient.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Services;
+
+use App\Models\MstMaternityQuestion;
+use App\Models\TblPatientMedium;
+use App\Models\TblPatientReview;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
+class TestCurlHTTPClient{
+
+}
