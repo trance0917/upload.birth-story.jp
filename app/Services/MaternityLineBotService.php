55cfc6d208db3cd26f51004f754dbc8cca72a188
hachiya <kei8@apost.plala.or.jp>
2023-12-11 00:52:35 +0900

    コミット

--- app/Services/MaternityLineBotService.php
+++ app/Services/MaternityLineBotService.php
@@ -144,7 +144,8 @@ class MaternityLineBotService extends LINEBot
                 if ($mst_maternity_user->is_review_notification) {
                     //通知を受けるべき点数の場合
                     if ($tbl_patient->average_score >= $this->mst_maternity->notification_review_score) {
-                        $this->pushMessage($mst_maternity_user->line_user_id, new TextMessageBuilder(view('lines/review-maternity-user', ['tbl_patient' => $tbl_patient,])->render()), $mst_maternity_user);
+                        $this->pushLineReviewHighRatingToMaternityUser($mst_maternity_user,$tbl_patient);
+//                        $this->pushMessage($mst_maternity_user->line_user_id, new TextMessageBuilder(view('lines/review-maternity-user', ['tbl_patient' => $tbl_patient,])->render()), $mst_maternity_user);
                     }
                 }
             }
@@ -156,75 +157,5 @@ class MaternityLineBotService extends LINEBot
         $this->pushMessage($tbl_patient->line_user_id, new TextMessageBuilder(view('lines/story-complete', ['tbl_patient' => $tbl_patient,])->render()), $tbl_patient);
     }
 
-    public function pushLineReviewPatientHighRating(TblPatient $tbl_patient){
-        $message = [
-            'type' => 'flex', 'altText' => '高評価ありがとうございます！',
-            'contents' => [
-                'type' => 'bubble', 'size' => 'kilo', 'direction' => 'ltr',
-                'body' => [
-                    'type' => 'box', 'layout' => 'vertical',
-                    'contents' => [
-                        ['type' => 'text', 'text' => '高評価ありがとうございます！', 'size' => 'md', 'weight' => 'bold', 'align' => 'center'],
-                        ['type' => 'separator', 'margin' => 'md'],
-                        [
-                            'type' => 'image',
-                            //todo 下記画像URLは変える必要がある
-                            'url' => 'https://dev.upload.birth-story.jp/images/line-star.png?sadf',
-                            'margin' => 'lg', 'offsetTop' => '0px', 'offsetBottom' => '0px', 'offsetStart' => '0px', 'offsetEnd' => '0px',
-                            'aspectRatio' => '5:1',
-                            'size' => '3xl'
-                        ],
-                        [
-                            'type' => 'text', 'text' => $tbl_patient->mst_maternity->name.'の高評価レビューをありがとうございます！よろしければこの内容をそのままgoogleに投稿いただけませんか？',
-                            'wrap' => true, 'color' => '#555555', 'size' => 'md', 'weight' => 'regular',
-                            'contents' => [
-                                [
-                                    'type' => 'span', 'text' => $tbl_patient->mst_maternity->name."の高評価レビューをありがとうございます！\nよろしければこの内容をそのまま"
-                                ],
-                                [
-                                    'type' => 'span', 'text' => 'googleの口コミに投稿',
-                                    'decoration' => 'underline', 'weight' => 'bold'
-                                ],
-                                ['type' => 'span', 'text' => 'いただけませんか？']
-                            ],
-                            'margin' => 'lg'
-                        ],
-                        [
-                            'type' => 'text',
-                            'text' => '下記のボタンから、レビューをコピーして30秒で投稿ができます。',
-                            'contents' => [
-                                ['type' => 'span', 'text' => '下記のボタンから、'],
-                                [
-                                    'type' => 'span',
-                                    'text' => 'レビューをコピーして30秒で投稿',
-                                    'decoration' => 'underline', 'weight' => 'bold'
-                                ],
-                                ['type' => 'span', 'text' => 'ができます。']
-                            ],
-                            'wrap' => true
-                        ]
-                    ],
-                    'spacing' => 'none', 'margin' => 'none'
-                ],
-                'footer' => [
-                    'type' => 'box', 'layout' => 'vertical',
-                    'contents' => [
-                        [
-                            'type' => 'button', 'style' => 'primary', 'color' => '#905c44', 'margin' => 'none',
-                            'action' => [
-                                'type' => 'uri', 'label' => 'コピーして投稿する',
-                                'uri' => route('guide',$tbl_patient)
-                            ],
-                            'height' => 'md'
-                        ]
-                    ],
-                    'margin' => 'none', 'spacing' => 'none'
-                ],
-                'styles' => ['footer' => ['separator' => true]]
-            ],
-        ];
 
-//        dump(json_encode($message));
-        $this->pushMessage($tbl_patient->line_user_id, new RawMessageBuilder($message), $tbl_patient);
-    }
 }
--- app/Services/Traits/MaternityLineBotServicePushMessageTrait.php
+++ app/Services/Traits/MaternityLineBotServicePushMessageTrait.php
@@ -3,6 +3,8 @@
 namespace App\Services\Traits;
 
 use App\Models\TblPatient;
+use App\Models\MstMaternityUser;
+use LINE\LINEBot\MessageBuilder\RawMessageBuilder;
 use LINE\LINEBot\RichMenuBuilder;
 use LINE\LINEBot\RichMenuBuilder\RichMenuAreaBoundsBuilder;
 use LINE\LINEBot\RichMenuBuilder\RichMenuAreaBuilder;
@@ -12,5 +14,146 @@ use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
 
 trait MaternityLineBotServicePushMessageTrait
 {
+    public function pushLineReviewPatientHighRating(TblPatient $tbl_patient){
+        $message = [
+            'type' => 'flex', 'altText' => '高評価ありがとうございます！',
+            'contents' => [
+                'type' => 'bubble', 'size' => 'kilo', 'direction' => 'ltr',
+                'body' => [
+                    'type' => 'box', 'layout' => 'vertical',
+                    'contents' => [
+                        ['type' => 'text', 'text' => '高評価ありがとうございます！', 'size' => 'md', 'weight' => 'bold', 'align' => 'center'],
+                        ['type' => 'separator', 'margin' => 'md'],
+                        [
+                            'type' => 'image',
+                            //todo 下記画像URLは変える必要がある
+                            'url' => 'https://dev.upload.birth-story.jp/images/line-star.png?sadf',
+                            'margin' => 'lg', 'offsetTop' => '0px', 'offsetBottom' => '0px', 'offsetStart' => '0px', 'offsetEnd' => '0px',
+                            'aspectRatio' => '5:1',
+                            'size' => '3xl'
+                        ],
+                        [
+                            'type' => 'text', 'text' => $tbl_patient->mst_maternity->name.'の高評価レビューをありがとうございます！よろしければこの内容をそのままgoogleに投稿いただけませんか？',
+                            'wrap' => true, 'color' => '#555555', 'size' => 'md', 'weight' => 'regular',
+                            'contents' => [
+                                [
+                                    'type' => 'span', 'text' => $tbl_patient->mst_maternity->name."の高評価レビューをありがとうございます！\nよろしければこの内容をそのまま"
+                                ],
+                                [
+                                    'type' => 'span', 'text' => 'googleの口コミに投稿',
+                                    'decoration' => 'underline', 'weight' => 'bold'
+                                ],
+                                ['type' => 'span', 'text' => 'いただけませんか？']
+                            ],
+                            'margin' => 'lg'
+                        ],
+                        [
+                            'type' => 'text',
+                            'text' => '下記のボタンから、レビューをコピーして30秒で投稿ができます。',
+                            'contents' => [
+                                ['type' => 'span', 'text' => '下記のボタンから、'],
+                                [
+                                    'type' => 'span',
+                                    'text' => 'レビューをコピーして30秒で投稿',
+                                    'decoration' => 'underline', 'weight' => 'bold'
+                                ],
+                                ['type' => 'span', 'text' => 'ができます。']
+                            ],
+                            'wrap' => true
+                        ]
+                    ],
+                    'spacing' => 'none', 'margin' => 'none'
+                ],
+                'footer' => [
+                    'type' => 'box', 'layout' => 'vertical',
+                    'contents' => [
+                        [
+                            'type' => 'button', 'style' => 'primary', 'color' => '#905c44', 'margin' => 'none',
+                            'action' => [
+                                'type' => 'uri', 'label' => 'コピーして投稿する',
+                                'uri' => route('guide',$tbl_patient)
+                            ],
+                            'height' => 'md'
+                        ]
+                    ],
+                    'margin' => 'none', 'spacing' => 'none'
+                ],
+                'styles' => ['footer' => ['separator' => true]]
+            ],
+        ];
 
+//        dump(json_encode($message));
+        $this->pushMessage($tbl_patient->line_user_id, new RawMessageBuilder($message), $tbl_patient);
+    }
+
+    public function pushLineReviewHighRatingToMaternityUser(MstMaternityUser $mst_maternity_user,TblPatient $tbl_patient){
+        $message = [
+            'type' => 'flex', 'altText' => '高評価ありがとうございます！',
+            'contents' => [
+                'type' => 'bubble', 'size' => 'kilo', 'direction' => 'ltr',
+                'body' => [
+                    'type' => 'box', 'layout' => 'vertical',
+                    'contents' => [
+                        ['type' => 'text', 'text' => '高評価ありがとうございます！', 'size' => 'md', 'weight' => 'bold', 'align' => 'center'],
+                        ['type' => 'separator', 'margin' => 'md'],
+                        [
+                            'type' => 'image',
+                            //todo 下記画像URLは変える必要がある
+                            'url' => 'https://dev.upload.birth-story.jp/images/line-star.png?sadf',
+                            'margin' => 'lg', 'offsetTop' => '0px', 'offsetBottom' => '0px', 'offsetStart' => '0px', 'offsetEnd' => '0px',
+                            'aspectRatio' => '5:1',
+                            'size' => '3xl'
+                        ],
+                        [
+                            'type' => 'text', 'text' => $tbl_patient->mst_maternity->name.'の高評価レビューをありがとうございます！よろしければこの内容をそのままgoogleに投稿いただけませんか？',
+                            'wrap' => true, 'color' => '#555555', 'size' => 'md', 'weight' => 'regular',
+                            'contents' => [
+                                [
+                                    'type' => 'span', 'text' => $tbl_patient->mst_maternity->name."の高評価レビューをありがとうございます！\nよろしければこの内容をそのまま"
+                                ],
+                                [
+                                    'type' => 'span', 'text' => 'googleの口コミに投稿',
+                                    'decoration' => 'underline', 'weight' => 'bold'
+                                ],
+                                ['type' => 'span', 'text' => 'いただけませんか？']
+                            ],
+                            'margin' => 'lg'
+                        ],
+                        [
+                            'type' => 'text',
+                            'text' => '下記のボタンから、レビューをコピーして30秒で投稿ができます。',
+                            'contents' => [
+                                ['type' => 'span', 'text' => '下記のボタンから、'],
+                                [
+                                    'type' => 'span',
+                                    'text' => 'レビューをコピーして30秒で投稿',
+                                    'decoration' => 'underline', 'weight' => 'bold'
+                                ],
+                                ['type' => 'span', 'text' => 'ができます。']
+                            ],
+                            'wrap' => true
+                        ]
+                    ],
+                    'spacing' => 'none', 'margin' => 'none'
+                ],
+                'footer' => [
+                    'type' => 'box', 'layout' => 'vertical',
+                    'contents' => [
+                        [
+                            'type' => 'button', 'style' => 'primary', 'color' => '#905c44', 'margin' => 'none',
+                            'action' => [
+                                'type' => 'uri', 'label' => 'コピーして投稿する',
+                                'uri' => route('guide',$tbl_patient)
+                            ],
+                            'height' => 'md'
+                        ]
+                    ],
+                    'margin' => 'none', 'spacing' => 'none'
+                ],
+                'styles' => ['footer' => ['separator' => true]]
+            ],
+        ];
+//        dump(json_encode($message));
+//        $this->pushMessage($tbl_patient->line_user_id, new RawMessageBuilder($message), $mst_maternity_user);
+    }
 }
