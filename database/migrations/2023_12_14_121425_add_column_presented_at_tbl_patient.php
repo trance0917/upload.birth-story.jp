ef37831766d67e990d8ebacacef23340fc2e570f
hachiya0917 <hachiya@e-metals.net>
2023-12-14 12:17:34 +0900

    コミット

---
+++ database/migrations/2023_12_14_121425_add_column_presented_at_tbl_patient.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tbl_patients', function (Blueprint $table) {
+            $table->timestamp('presented_at')->nullable()->default(null)->after('completed_at')->comment('プレゼント手続きを行った日時');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tbl_patients', function (Blueprint $table) {
+            $table->dropColumn('presented_at');
+        });
+    }
+};
