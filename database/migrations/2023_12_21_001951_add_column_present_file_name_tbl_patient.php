6d7f2995e6bf524f61d8d3aa52426db4f28ab405
hachiya <kei8@apost.plala.or.jp>
2023-12-21 00:25:10 +0900

    commit

---
+++ database/migrations/2023_12_21_001951_add_column_present_file_name_tbl_patient.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tbl_patients', function (Blueprint $table) {
+            $table->string('present_file_name', 255)->after('presented_at')->comment('プレゼント用のファイル名');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tbl_patients', function (Blueprint $table) {
+            //カラムの削除
+            $table->dropColumn('present_file_name');
+        });
+    }
+};
