5e541abb26ac9ef7b84a09959452fdafdc059c41
kei8 <kei8@apost.plala.or.jp>
2024-06-24 19:58:56 +0900

    commit

--- app/Http/Controllers/Api/MaternityController.php
+++ app/Http/Controllers/Api/MaternityController.php
@@ -10,11 +10,16 @@ use App\Models\TblLiff;
 class MaternityController extends Controller
 {
     public function index(Request $request,MaternityService $maternity_service) {
-        $tbl_liff = new TblLiff;
-        $tbl_liff->mst_maternity_id = $request->mst_maternity_id;
-        $tbl_liff->line_user_id = $request->line_user_id;
-        $tbl_liff->save();
 
+        $m = TblLiff::where('line_user_id', $request->line_user_id)
+            ->where('mst_maternity_id', $request->mst_maternity_id)
+            ->orderByDesc('tbl_liff_id')->first();
+        if(!$m){
+            $tbl_liff = new TblLiff;
+            $tbl_liff->mst_maternity_id = $request->mst_maternity_id;
+            $tbl_liff->line_user_id = $request->line_user_id;
+            $tbl_liff->save();
+        }
         //todo: 規定評価以上なら産院にメッセージを送る
         return response()->json([
             'result' => true,
