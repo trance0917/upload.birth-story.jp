972521cafa34c499e9e8c6e79c272f05d5865629
kei8 <kei8@apost.plala.or.jp>
2024-06-24 16:28:20 +0900

    commit

--- app/Http/Controllers/Api/MaternityController.php
+++ app/Http/Controllers/Api/MaternityController.php
@@ -11,9 +11,10 @@ class MaternityController extends Controller
 {
     public function index(Request $request,MaternityService $maternity_service) {
         $tbl_liff = new TblLiff;
-        $tbl_liff->test = $request->tbl_maternity_id;
+        $tbl_liff->tbl_maternity_id = $request->tbl_maternity_id;
         $tbl_liff->line_user_id = $request->line_user_id;
         $tbl_liff->save();
+
         //todo: 規定評価以上なら産院にメッセージを送る
         return response()->json([
             'result' => true,
