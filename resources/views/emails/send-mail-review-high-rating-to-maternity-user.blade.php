a5aa7665d7d4328196a9805ae7c899085540766e
kei8 <kei8@apost.plala.or.jp>
2024-08-22 23:54:31 +0900

    commit

--- resources/views/emails/send-mail-review-high-rating-to-maternity-user.blade.php
+++ resources/views/emails/send-mail-review-high-rating-to-maternity-user.blade.php
@@ -3,6 +3,7 @@
 ─────────────────────────────
 [アンケートの店舗]
 {{$tbl_patient->mst_maternity->name}}
+
 [回答したお客さま]
 {{$tbl_patient->line_name}}さま
 
@@ -14,6 +15,7 @@
 @foreach($tbl_patient->tbl_patient_reviews AS $tbl_patient_review_key => $tbl_patient_review)
 [{{($tbl_patient_review_key+1)}}]{{$tbl_patient_review->mst_maternity_question->question}}
 {{(String)$tbl_patient_review->score}}点
+
 @endforeach
 
 [レビュー]
