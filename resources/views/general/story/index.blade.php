b79aece33d2a3544b25de79b6abe6cec9fca09b7
kei8 <kei8@apost.plala.or.jp>
2026-03-19 16:54:47 +0900

    commit

--- resources/views/general/story/index.blade.php
+++ resources/views/general/story/index.blade.php
@@ -51,7 +51,7 @@
 
     ">
         <li><a class="bg-[#00B900]" href="https://lin.ee/{{config('const.line_official_id')}}">LINEお問い合わせ</a></li>
-        <li><a class="bg-slate-400" href="/faq?submission_type=@{{tbl_patient.mst_maternity.submission_type}}" target="_blank">よくある質問</a></li>
+        <li><a class="bg-slate-400" href="/faq?submission_type={{tbl_patient.mst_maternity.submission_type}}" target="_blank">よくある質問</a></li>
     </ul>
 
 
