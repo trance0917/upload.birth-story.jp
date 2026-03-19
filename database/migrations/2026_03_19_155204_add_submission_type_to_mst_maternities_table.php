8f5e81d11ffe10fbb65eea5b73481bda225f61ae
kei8 <kei8@apost.plala.or.jp>
2026-03-19 15:56:16 +0900

    commit

---
+++ database/migrations/2026_03_19_155204_add_submission_type_to_mst_maternities_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mst_maternities', function (Blueprint $table) {
+            // is_case の次に int型の submission_type を追加
+            $table->integer('submission_type')
+                ->nullable() // ※既存レコードがある場合エラーにならないようにnullableにしています
+                ->after('is_case')
+                ->comment('提出タイプ');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mst_maternities', function (Blueprint $table) {
+            // ロールバック用にカラムの削除処理を記述
+            $table->dropColumn('submission_type');
+        });
+    }
+};
